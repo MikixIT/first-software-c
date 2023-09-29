41a4d4b259ab81852502d7bdeb1dc506aa31840c
MikixIT <michael.46@live.it>
2023-09-29 17:21:43 +0200

    first commit, addition software in c

---
+++ first-c-software/main.c
@@ -0,0 +1,24 @@
+//
+//  main.c
+//  first-c-software
+//
+//  Created by Michael Torres on 29/09/23.
+//
+
+#include <stdio.h>
+
+int main(void) {
+    int num1, num2, sum;
+    
+    printf("Hey, let's do some addition\n ");
+    printf("Enter the first number: ");
+    scanf("%d", &num1);
+    
+    printf("Alright, enter the second number now: ");
+    scanf("%d", &num2);
+    
+    sum=num1+num2;
+    printf("Okay, the sum of %d and %d is: %d\n", num1, num2, sum);
+    printf("\nHave a good day! by Michael :)\n\n");
+    return 0;
+}
